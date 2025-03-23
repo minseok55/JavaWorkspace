43c6a58df9da0d201b9944a1c32af8ba1b95e3cb
Song min seok <mseoki55@gmail.com>
2025-03-23 23:12:44 +0900

    chap_04
    if_1 완료

--- src/chap_04/_04_SwitchCase.java
+++ src/chap_04/_04_SwitchCase.java
@@ -23,7 +23,6 @@ public class _04_SwitchCase {
         System.out.println("조회 완료 #1");
 
         // Switch Case 문을 이용
-
         ranking = 5;
         switch (ranking) {
             case 1:
@@ -39,7 +38,36 @@ public class _04_SwitchCase {
                 System.out.println("장학금 대상 아님");
         }
         System.out.println("조회 완료 #2");
-    }
 
+
+        // case 2 와 3 을 통합
+        ranking = 3;
+        switch (ranking) {
+            case 1:
+                System.out.println("전액 장학금");
+                break;
+            case 2:
+            case 3:
+                System.out.println("반액 장학금");
+                break;
+            default:
+                System.out.println("장학금 대상 아님");
+        }
+        System.out.println("조회 완료 #3");
+
+        // 중고상품의 등급에 따른 가격을 책정 (1급 : 최상, 4급 : 최하)
+        int grade = 2; // 등급
+        int price = 7000; // 기본 가격
+        switch (grade) {
+            case 1:
+                price += 1000; // price = price + 1000;
+            case 2:
+                price += 1000;
+            case 3:
+                price += 1000;
+                break;
+        }
+        System.out.println(grade + "등급 제품의 가격 : " + price + "원");
+    }
 }
 
