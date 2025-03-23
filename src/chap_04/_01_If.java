cd1ea04c44e2e96781ae5dbc5d339ff9293db233
Song min seok <mseoki55@gmail.com>
2025-03-23 19:06:34 +0900

    chap_04
    if_1 완료

--- src/chap_04/_01_If.java
+++ src/chap_04/_01_If.java
@@ -4,6 +4,13 @@ public class _01_If {
     public static void main(String[] args) {
         //조건문
         int hour = 15; // 오전 10시
+
+        // if 문 내에서 하나의 문장을 실행할 때는 { } 생략가능
+        if (hour < 14) {
+            System.out.println("아이스아메리카노 + 1");
+        }
+
+        //if 문 내에서 2개 이상의 문장을 실행할 때는 { } 생략 불가
         if (hour < 14) {
             System.out.println("아이스아메리카노 + 1");
             System.out.println("샷추가");
