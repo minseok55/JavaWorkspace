882512542f203edd003d391f636984a8791339b9
Song min seok <mseoki55@gmail.com>
2025-03-23 19:03:24 +0900

    chap_04
    패키지 이름 수정

--- src/chap_04/_01_If.java
+++ src/chap_04/_01_If.java
@@ -2,6 +2,11 @@ package chap_04;
 
 public class _01_If {
     public static void main(String[] args) {
-
+        //조건문
+        int hour = 15; // 오전 10시
+        if (hour < 14)
+            System.out.println("아이스아메리카노 + 1");
+        System.out.println("샷추가");
+        System.out.println("커피 주문 완료");
     }
 }
