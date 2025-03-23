365db64fb887865bf203c7fe444726c6dbef132a
Song min seok <mseoki55@gmail.com>
2025-03-23 19:04:24 +0900

    chap_04
    if 추가

--- src/chap_04/_01_If.java
+++ src/chap_04/_01_If.java
@@ -4,9 +4,10 @@ public class _01_If {
     public static void main(String[] args) {
         //조건문
         int hour = 15; // 오전 10시
-        if (hour < 14)
+        if (hour < 14) {
             System.out.println("아이스아메리카노 + 1");
-        System.out.println("샷추가");
+            System.out.println("샷추가");
+        }
         System.out.println("커피 주문 완료");
     }
 }
