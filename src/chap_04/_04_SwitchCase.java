d470f06f9d5c635c04b2b6fe8b1ffbf730e74f49
Song min seok <mseoki55@gmail.com>
2025-03-23 19:45:44 +0900

    chap_04
    if_1 완료

---
+++ src/chap_04/_04_SwitchCase.java
@@ -0,0 +1,45 @@
+package chap_04;
+
+public class _04_SwitchCase {
+    public static void main(String[] args) {
+        // Switch case
+        // 석차에 따른 장학금 지급
+        // 1등 : 전액 장학금
+        // 2등 : 반액 장학금
+        // 3등 : 반액 장학금
+        // 그 외 : 장장학금 대상 아님
+
+        // If Else 문을 이용한 방법
+        int ranking = 2; // 1등
+        if (ranking == 1) {
+            System.out.println("전액 장학금");
+        } else if (ranking == 2 || ranking == 3) {
+            System.out.println("반액 장학금");
+//        } else if (ranking == 3) {
+//            System.out.println("반액 장학금");
+        } else {
+            System.out.println("장학금 대상 아님");
+        }
+        System.out.println("조회 완료 #1");
+
+        // Switch Case 문을 이용
+
+        ranking = 5;
+        switch (ranking) {
+            case 1:
+                System.out.println("전액 장학금");
+                break;
+            case 2:
+                System.out.println("반액 장학금");
+                break;
+            case 3:
+                System.out.println("반액 장학금");
+                break;
+            default:
+                System.out.println("장학금 대상 아님");
+        }
+        System.out.println("조회 완료 #2");
+    }
+
+}
+
