dacddf85b2718c2b9638c44a27d5c26c96c25ff7
Song min seok <mseoki55@gmail.com>
2025-03-25 21:50:13 +0900

    chap_06
    Method, Return...

--- src/chap_06/_01_Method.java
+++ src/chap_06/_01_Method.java
@@ -1,7 +1,7 @@
 package chap_06;
 
 public class _01_Method {
-    // 메소드 정의
+    // 메소드 정의 (메소드명은 주로 동사를 사용) : 어떤 기능을 하는 코드들의 묶음
     public static void sayHello() { // 동작 => 주로 동사를 사용
         System.out.println("안녕하세요 메소드입니다.");
     }
