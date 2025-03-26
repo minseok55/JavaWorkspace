af52d65f7705263ba777de3ee6f2056b642dddf4
Song min seok <mseoki55@gmail.com>
2025-03-26 22:50:17 +0900

    chap_07 Class
    Class Method

--- src/chap_07/BlackBox.java
+++ src/chap_07/BlackBox.java
@@ -9,7 +9,27 @@ public class BlackBox {
 
     String color; // 색상
 
+    int serialNumber; // 시리얼 번호
+
+    static int counter = 0; // 시리얼 번호를 생성해주는 역할 (처음엔 0이었다가 ++ 연산을 통해서 값을 증가)
+
     static boolean canAutoReport = false; // 자동 신고 기능 // 클래스 내의 클래스 변수
+
+    BlackBox() {
+        System.out.println("기본 생성자 호출");
+        this.serialNumber = ++counter;
+        System.out.println("새로운 시리얼 넘버를 발급받았습니다 : " + this.serialNumber);
+    }
+
+    BlackBox(String modelName, String resolution, int price, String color) {
+        this(); // 기본 생성자 호출
+        System.out.println("사용자 정의 생성자 호출");
+        this.modelName = modelName;
+        this.resolution = resolution;
+        this.price = price;
+        this.color = color;
+
+    }
 
     void autoReport() {
         if (canAutoReport) {
@@ -30,5 +50,33 @@ public class BlackBox {
             return 1;
         }
         return 10;
+    }
+
+    // showDateTime : 날짜정보 표시여부
+    // showSpeed : 속도정보 표시여부
+    // min : 영상 기록 단위(분)
+    void record(boolean showDateTime, boolean showSpeed, int min) {
+        System.out.println("녹화를 시작합니다.");
+        if (showDateTime) {
+            System.out.println("영상에 날짜정보가 표시됩니다.");
+        }
+        if (showSpeed) {
+            System.out.println("영상에 속도정보가 표시됩니다.");
+        }
+        System.out.println("영상은 " + min + "분 단위로 기록됩니다.");
+    }
+
+    void record() {
+        record(true, true, 5);
+    }
+
+    static void callServiceCenter() {
+        System.out.println("서비스 센터(1588-0000) 로 연결합니다.");
+        // modelName = "test"; // 직접 접근이 불가능
+        // canAutoReport = false; // 스태틱으로 선언한 클래스 변수는 스태틱 메소드에서 바로 사용 가능
+    }
+
+    void appendModelName(String modelName) {
+        this.modelName += modelName;
     }
 }
