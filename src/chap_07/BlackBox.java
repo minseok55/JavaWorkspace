94f57df7e78442354d62a035390fb011764cba8b
Song min seok <mseoki55@gmail.com>
2025-03-26 20:51:11 +0900

    chap_07 Class
    Method, Return...

---
+++ src/chap_07/BlackBox.java
@@ -0,0 +1,34 @@
+package chap_07;
+
+public class BlackBox {
+    String modelName; // 모델명 // 인스턴스 변수
+
+    String resolution; //해상도
+
+    int price; // 가격
+
+    String color; // 색상
+
+    static boolean canAutoReport = false; // 자동 신고 기능 // 클래스 내의 클래스 변수
+
+    void autoReport() {
+        if (canAutoReport) {
+            System.out.println("충돌이 감지되어 자동으로 신고합니다.");
+        } else {
+            System.out.println("자동 신고 기능이 지원되지 않습니다.");
+        }
+    }
+
+    void insertMemoryCard(int capacity) {
+        System.out.println("메상량은 " + capacity + "GB 입니다.");
+    }
+
+    int getVideoFileCount(int type) {
+        if (type == 1) { // 일반 영상
+            return 9;
+        } else if (type == 2) { // 이벤트 영
+            return 1;
+        }
+        return 10;
+    }
+}
