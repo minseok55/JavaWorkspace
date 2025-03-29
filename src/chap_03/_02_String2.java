61172f4e9b1890c228022951d8b1a700d94bdbb7
Song min seok <mseoki55@gmail.com>
2025-03-29 23:24:17 +0900

    chap_02
    문자열 복습
    문자열의 길이, 대소문자 변환, 문자열 변환 , 공백 제거, 문자열 결합, 문자열 비교, 특수문자

--- src/chap_03/_02_String2.java
+++ src/chap_03/_02_String2.java
@@ -5,11 +5,13 @@ public class _02_String2 {
         String s = "I like Java and Python and C.";
 
         // 문자열 변환
-        System.out.println(s.replace(" and",",")); // " and" 를 "," 로 변환
+        System.out.println(s.replace(" and", ",")); // " and" 를 "," 로 변환
         System.out.println(s.substring(7)); // 인덱스 기준 7 부터 시작 (이전 내용은 삭제)
-        System.out.println(s.substring(s.indexOf("Java")));
+        System.out.println(s.substring(s.indexOf("Java"))); // 시작위치만 지정 할 경우
         // "Java" 가 시작하는 위치부터, "." 이 시작하는 위치 바로 앞까지
         System.out.println(s.substring(s.indexOf("Java"), s.indexOf("."))); // 시작 위치부터 끝 위치 "직전" 까지
+        System.out.println(s.substring(s.indexOf("Java"), s.lastIndexOf("and")));
+        // 시작 위치부터 마지막에 일치하는 문자열 "직전" 까지
 
         // 공백 제거
         s = "      I love Java.       ";
@@ -22,6 +24,7 @@ public class _02_String2 {
         System.out.println(s1 + s2); // JavaPython
         System.out.println(s1 + "," + s2); // Java,Python
         System.out.println(s1.concat(",").concat(s2)); //Java,Python
+        // s1 문자열에 "," 를 더하기 * .concat() 괄호 안의 문자 및 변수를 더하기
 
 
     }
--- src/chap_03/_03_StringCompare.java
+++ src/chap_03/_03_StringCompare.java
@@ -6,8 +6,8 @@ public class _03_StringCompare {
         String s1 = "Java";
         String s2 = "Python";
 
-        System.out.println(s1.equals("Java")); // 문자열 내용이 같으면 true, 다르면 false
-        System.out.println(s2.equals("python")); // 대소문자 구분
+        System.out.println(s1.equals("Java")); // s1변수의 문자열 내용이 ()안의 문자와 같으면 true, 다르면 false
+        System.out.println(s2.equals("python")); // 대소문자 구분 * 대소문자가 다르면 false
 
         // 대소문자 구분 없이 문자열 내용이 같은지 체크
         System.out.println(s2.equalsIgnoreCase("python"));
@@ -15,13 +15,15 @@ public class _03_StringCompare {
         // 문자열 비교 심화
         s1 = "1234"; // 벽에 붙은 메모지의 비밀번호 정보 (참조)
         s2 = "1234";
-        System.out.println(s1.equals(s2)); // true (내용)
+        System.out.println(s1.equals(s2)); // true (내용) * s1 의 내용과 s2의 내용을 비교
         System.out.println(s1 == s2); // true (참조)
+        // 메모리의 s1 공간에 저장된 1234를 같은 메모리공간에서 s2 에게 부여
 
-        s1 = new String("1234");
+        s1 = new String("1234"); // 각각의 서로 다른 메모리
         s2 = new String("1234");
         System.out.println(s1.equals(s2)); // true (내용)
         System.out.println(s1 == s2); // false (참조)
-
+        // s1의 변수와 s2의 변수는 서로 다른 메모리공간에 각각 저장 되어있음
+        // Java 에서 일반적으로 문자열의 내용을 비교할 경우 equals 사용
     }
 }
