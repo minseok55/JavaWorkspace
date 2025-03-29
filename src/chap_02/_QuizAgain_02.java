ad512a552dae43f33cdef6bdf4a9f9b572f7e0b0
Song min seok <mseoki55@gmail.com>
2025-03-29 18:44:45 +0900

    chap_02
    연산자 복습
    산술, 대입, 비교, 논리. 삼항
    주석 공부법 복습

--- src/chap_02/_QuizAgain_02.java
+++ src/chap_02/_QuizAgain_02.java
@@ -15,9 +15,15 @@ package chap_02;
 
 public class _QuizAgain_02 {
     public static void main(String[] args) {
-        int height = 120;
-        int a = 120;
-        String b = (a >= height) ? "탑승 가능합니다." : "탑승 불가능합니다";
-        System.out.println("키가 " + a + " 이므로 " + b);
+        int height = 120; // 키 기준
+        int a = 130; // 어린이 키
+        String result = (a >= height) ? "탑승 가능합니다." : "탑승 불가능합니다";
+        //어린이 키가 기준보다 크기 때문에 : 왼쪽 "탑승 가능합니다" 변수 b에 입력 됨
+        System.out.println("키가 " + a + "cm 이므로 " + result);
+
+        // 해설
+//        int hight = 115;
+//        String result = (조건) ? (참) : (거짓);
+//        System.out.println("키가 " + "cm 이므로" + result);
     }
 }
