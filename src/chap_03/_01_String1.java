5ed5c7a74a97603d8cdccc38555167533e60ebd5
Song min seok <mseoki55@gmail.com>
2025-03-29 23:24:17 +0900

    chap_02
    연산자 복습
    산술, 대입, 비교, 논리. 삼항
    주석 공부법 복습

--- src/chap_03/_01_String1.java
+++ src/chap_03/_01_String1.java
@@ -6,20 +6,22 @@ public class _01_String1 {
         System.out.println(s);
 
         // 문자열의 길이
-        System.out.println(s.length()); // 29
+        System.out.println(s.length()); // 29, s 변수의 문자열 길이 확인 length
+        // 리뷰 작성 시 ()글자 이상으로 입력하세요 같은 요청을 할때 많이 쓰임
 
         // 대소문자 변환
         System.out.println(s.toUpperCase()); // 대문자로 변경
         System.out.println(s.toLowerCase()); // 소문자로 변경
 
         // 포함 관계
-        System.out.println(s.contains("Java")); // 포함된다면 true
+        System.out.println(s.contains("Java")); // () 안의 문자가 포함된다면 true / contains
         System.out.println(s.contains("C#")); // 포함되지 않는다면 false
-        System.out.println(s.indexOf("Java")); // 위치 정보 7
+        System.out.println(s.indexOf("Java")); // 위치 정보 7 , ()안의 문자열이 시작되는 위치 (J) / indexOf
+        // 컴퓨터에서 문자열 시작 위치는 1이 아니라 0 부터 시작
         System.out.println(s.indexOf("C#")); // 포함되지 않는다면 -1
-        System.out.println(s.indexOf("and")); // 처음 일치하는 위치 정보 (12)
-        System.out.println(s.lastIndexOf("and")); // 마지막에 일치하는 위치 정보 (23)
-        System.out.println(s.startsWith("I like")); // 이 문자열로 시작하면 true (아니면 false)
-        System.out.println(s.endsWith(".")); // 이 문자열로 끝나면 true (아니면 false)
+        System.out.println(s.indexOf("and")); // () 안의 같은 문자열이 여러개 인 경우 처음 일치하는 위치 정보 (12)
+        System.out.println(s.lastIndexOf("and")); // 같은 문자열 중 마지막에 일치하는 위치 정보 (23) / lastIndexOf
+        System.out.println(s.startsWith("I like")); // 이 문자열로 시작하면 true (아니면 false) / startsWith
+        System.out.println(s.endsWith(".")); // 이 문자열로 끝나면 true (아니면 false) / endWith
     }
 }
--- src/chap_03/_04_EscapeSequence.java
+++ src/chap_03/_04_EscapeSequence.java
@@ -20,17 +20,19 @@ public class _04_EscapeSequence {
         System.out.println("부추전\t\t8000원");
 
         // \\ : 역슬래시
-        System.out.println("C:\\Program Files\\Java"); // windows os
+        System.out.println("C:\\Program Files\\Java"); // windows os 기준 파일 경로를 표시
+        System.out.println("user/Java/JavaProject"); // mac os 기준
 
         // \" : 큰따옴표
         // 단비가 "냐옹" 이라고 했어요
         System.out.println("단비가 \"냐옹\" 이라고 했어요");
+        // System.out.println("단비가 "냐옹" 이라고 했어요"); 오류. ""가 겹치기 때문에 \" 을 사용하여 "냐옹" 으로 출력
 
         // \' : 작은따옴표
         // 단비가 '뭘 봐?' 라는 표정을 지었어요
         System.out.println("단비가 \'뭘 봐?\' 라는 표정을 지었어요");
-        System.out.println("단비가 '뭘 봐?' 라는 표정을 지었어요");
-
+        System.out.println("단비가 '뭘 봐?' 라는 표정을 지었어요"); // \ 가 없어도 문자열 표현에는 문제 없음
+        // 하나의 문자열을 저장하는 char 에 사용함
         char c = 'A';
         c = '\'';
         System.out.println(c);
