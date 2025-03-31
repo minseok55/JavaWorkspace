b091864356d76ae9dc4a117357b1b0c401acff09
Song min seok <mseoki55@gmail.com>
2025-03-31 22:48:10 +0900

    chap_05
    Array(배열), ArrayLoop(배열을 이용한 반복문),MultidimensionalArray(다차원 배열 복습

--- src/chap_05/_02_ArrayLoop.java
+++ src/chap_05/_02_ArrayLoop.java
@@ -10,6 +10,7 @@ public class _02_ArrayLoop {
 
         // for 반복문 순회
         for (int i = 0; i < 4; i++) { // 배열의 요소(숫자) for 반복문
+            // i 가 0부터 4직전까지 순회 하는 동안 그 값을 배열의 [ ] 속에 넣어서 0 ~ 4 직전까지 반복
             System.out.println(coffees[i] + " 하나");
         }
         System.out.println("주세요");
@@ -26,8 +27,9 @@ public class _02_ArrayLoop {
 
         System.out.println("---------------------------------------");
 
-        // enhanced for (for - each) 반복문
-        for (String coffee : coffees) { // coffees 라는 배열에 있는 값들을 순서대로 순회 하는데 그때 순회되는 값을 coffee 라는 이름으로 받아서 사용
+        // enhanced for (for - each) 반복문, 향상된 for 문
+        // coffees 라는 배열에 있는 값들을 순서대로 순회 하는데 그때 순회되는 값을 coffee 라는 이름으로 받아서 사용
+        for (String coffee : coffees) { // index 값이 없으므로 처음부터 끝까지 순서대로 순회
             System.out.println(coffee + " 하나");
         }
         System.out.println("주세요");
--- src/chap_05/_03_MultiArray.java
+++ src/chap_05/_03_MultiArray.java
@@ -2,20 +2,21 @@ package chap_05;
 
 public class _03_MultiArray {
     public static void main(String[] args) {
-        // 다차원 배열 (2차원 배열)
+        // 다차원 배열 (2차원 배열) , MultidimensionalArray
 
         // 소규모 영화관 좌석
         // A1 - A5
         // B1 - B5
         // C1 - C5
 
-        String[] seatA = {"A1", "A2", "A3", "A4", "A5"};
+        // 1차원 배열을 이용한 방법
+        String[] seatA = {"A1", "A2", "A3", "A4", "A5"}; // 한번에 영역을 선택하는 단축기 Option + 드래그
         String[] seatB = {"B1", "B2", "B3", "B4", "B5"};
         String[] seatC = {"C1", "C2", "C3", "C4", "C5"};
 
         // 3 x 5 크기의 2차원 배열
-        String[][] seats = new String[][]{
-                {"A1", "A2", "A3", "A4", "A5"},
+        String[][] seats = new String[][]{ // 2차원 배열 [][] 대괄호 2개, 차원이 늘어날수록 대괄고 1개씩 추가
+                {"A1", "A2", "A3", "A4", "A5"}, // 1차원 배열 값들을 , 구분한 다음 {} 감싸는 형태
                 {"B1", "B2", "B3", "B4", "B5"},
                 {"C1", "C2", "C3", "C4", "C5"}
         };
@@ -26,6 +27,8 @@ public class _03_MultiArray {
         // C5 에 접근 하려면?
         System.out.println(seats[2][4]);
 
+        System.out.println("-----------------------------------");
+
         // 첫 줄에는 3칸, 둘쨰 줄에는 4칸, 셋째 줄에는 5칸
         String[][] seats2 = {
                 {"A1", "A2", "A3"},
@@ -35,7 +38,7 @@ public class _03_MultiArray {
 
         // A3 에 접근 하려면?
         System.out.println(seats2[0][2]);
-
+        
         // A5 에 접근 하려면?
         // System.out.println(seats2[0][4]); (index 값 오류)
 
